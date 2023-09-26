f281a34aa9213daf50a1a62d67273caec703e763
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-27 00:45:59 +0800

    add home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home/en', pathMatch: 'full' },
+  { path: 'home', redirectTo: '/home/en', pathMatch: 'full' },
+  { path: 'home/:lang', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '../../services/translation/translate.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(private route: ActivatedRoute, private titleService: Title, private translationService: TranslateService) { }
+  async ngOnInit(): Promise<void> {
+    await this.setWebsiteTitle();
+  }
+  private async setWebsiteTitle() {
+    const lang = this.route.snapshot.paramMap.get('lang');
+    var website_title = await this.translationService.getTranslation(lang, "website_title");
+    this.titleService.setTitle(website_title);
+  }
 }
