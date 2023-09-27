b63c223e51a438bab50ffc4948fd9baa0c862117
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-09-27 08:55:26 +0800

    add auto adjust to device lang on home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { TranslateService } from '../../services/translation/translate.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
@@ -11,7 +11,7 @@ import { LoaderService } from 'src/app/services/loader/loader.service';
 })
 export class HomeComponent {
   constructor(private route: ActivatedRoute, private titleService: Title, private translationService: TranslateService,
-    private loader: LoaderService) { }
+    private loader: LoaderService, private router: Router) { }
   async ngOnInit(): Promise<void> {
     this.loader.setLoading(true);
     await this.setWebsiteTitle();
@@ -19,6 +19,13 @@ export class HomeComponent {
   }
   private async setWebsiteTitle() {
     const lang = this.route.snapshot.paramMap.get('lang');
+    console.log("parameter lang: " + lang);
+    const userLocale = navigator.languages && navigator.languages.length
+      ? navigator.languages[0]
+      : navigator.language;
+    console.log("device lang: " + userLocale);
+    if (lang?.toLowerCase() != userLocale.substring(0, 2).toLowerCase())
+      this.router.navigate(['/home/' + userLocale.substring(0, 2).toLowerCase()]);
     var website_title = await this.translationService.getTranslation(lang, "website_title");
     this.titleService.setTitle(website_title);
   }
