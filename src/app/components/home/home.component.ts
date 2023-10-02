553600fe891dddf687b2af504b18dd832c7a9b90
Dannel Lim Khye Hsen <dannel_lim@hotmail.com>
2023-10-03 06:26:05 +0800

    add language bar language change feature

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { LoaderService } from 'src/app/services/loader/loader.service';
 
 @Component({
@@ -13,20 +13,12 @@ export class HomeComponent {
   constructor(private route: ActivatedRoute, private titleService: Title,
     private loader: LoaderService, private router: Router, private translate: TranslateService) { }
   async ngOnInit(): Promise<void> {
-    this.loader.setLoading(true);
-    await this.setWebsiteTitle();
-    this.loader.setLoading(false);
-  }
-  websiteHeader: string = "";
-  private async setWebsiteTitle() {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    this.translate.setDefaultLang(this.translate.getBrowserLang()!);
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    this.translate.use(this.translate.getBrowserLang()!);
-
-    this.translate.get('WEBSITE_TITLE').subscribe((res: string) => {
-      this.titleService.setTitle(res);
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.loader.setLoading(true);
+      this.translate.get('WEBSITE_TITLE').subscribe((res: string) => {
+        this.titleService.setTitle(res);
+        this.loader.setLoading(false);
+      });
     });
-    //this.websiteHeader = await this.translationService.getTranslation(lang, "website_header");
   }
 }
--- src/app/components/language-bar/language-bar.component.ts
+++ src/app/components/language-bar/language-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-language-bar',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./language-bar.component.css']
 })
 export class LanguageBarComponent {
-
+  constructor(private translate: TranslateService, private config: ConfigService) {
+    this.selectLang = this.translate.getBrowserLang()?? "en";
+    // this language will be used as a fallback when a translation isn't found in the current language
+    this.translate.setDefaultLang(this.selectLang);
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    this.translate.use(this.selectLang);
+  }
+  selectLang: string = "";
+  TransLang: string[] = [];
+  setTransLanguage() {
+    this.translate.use(this.selectLang);
+  }
+  getTransLanguage() {
+    this.TransLang = this.config.getSupportedLanguages();
+  }
+  ngOnInit() {
+    this.getTransLanguage();
+  }
 }
